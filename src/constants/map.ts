1e4fb8b1b8310e515b9293fae78f8c6fb6935a61
Rihards <9032445+fandul@users.noreply.github.com>
2026-07-08 21:05:13 +0200

    feat: add env variables

---
+++ src/constants/map.ts
@@ -0,0 +1,5 @@
+export const MAP_EMBED_URL =
+  'https://www.google.com/maps/d/embed?mid=1hJKLQnok03vpbWouY7JqAaapqYc_BPc&ehbc=2E312F';
+
+export const MAP_VIEWER_URL =
+  'https://www.google.com/maps/d/viewer?mid=1hJKLQnok03vpbWouY7JqAaapqYc_BPc';
